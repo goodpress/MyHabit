e9bf97017bc85b2598fd5c148b989ddfcdcad8ca
weiyuke <weiyk@xinguangnet.com>
2018-05-09 14:12:52 +0800

    使用  网易  airtest , update ,with exception sences

--- airTest/single_section.air/single_section.py
+++ airTest/single_section.air/single_section.py
@@ -12,6 +12,15 @@ from airtest.core.api import *
 
 auto_setup(__file__)
 
+def backToHomeTabUseKeyEvent():
+    pluseIcon=exists(Template(r"tpl1525780162891.png", record_pos=(0.247, 0.692), resolution=(1080, 1920)))
+    touchedNum=0;
+    while pluseIcon == False:
+        keyevent("BACK")
+        pluseIcon=exists(Template(r"tpl1525780162891.png", record_pos=(0.247, 0.692), resolution=(1080, 1920)))
+        
+
+
 
 def backToHomeTab():
     pluseIcon=exists(Template(r"tpl1525780162891.png", record_pos=(0.247, 0.692), resolution=(1080, 1920)))
@@ -65,10 +74,10 @@ def cleanJunkAccount():
     touch(Template(r"tpl1525678744263.png", record_pos=(-0.328, -0.183), resolution=(1080, 1920)))
     sleep(2)
 
-    cacellationBtn = exists(Template(r"tpl1525774549108.png", record_pos=(-0.154, -0.14), resolution=(1080, 1920)))
+    cacellationBtn = exists(Template(r"tpl1525774549108.png", threshold=0.95, target_pos=5, rgb=False, record_pos=(-0.154, -0.14), resolution=(1080, 1920)))
     while cacellationBtn:
         print "this account is cacellationBtn" + str(cacellationBtn[0]) + "," + str(cacellationBtn[1])
-        touch(Template(r"tpl1525774549108.png", record_pos=(-0.154, -0.14), resolution=(1080, 1920)),duration=1)
+        touch(Template(r"tpl1525774549108.png", threshold=0.95, target_pos=5, rgb=False, record_pos=(-0.154, -0.14), resolution=(1080, 1920)),duration=1)
     
         touch(Template(r"tpl1525773678520.png", record_pos=(0.216, -0.691), resolution=(1080, 1920)))
         sleep(1)
@@ -80,13 +89,20 @@ def cleanJunkAccount():
     return;
 
 
- 
-def shareToSubAccount():
-    cleanJunkAccount();
-    
-    touch(Template(r"tpl1525451310507.png", record_pos=(0.209, -0.613), resolution=(1080.0, 1920.0)))
-    sleep(3)
 
+def cleanLocation():
+    isLoacationAlert = exists(Template(r"tpl1525799111409.png", threshold=0.9, target_pos=5, rgb=False, record_pos=(0.076, -0.061), resolution=(1080, 1920)))
+    while isLoacationAlert:
+        cancelBtn=exists(Template(r"tpl1525799136428.png", threshold=0.9, target_pos=5, rgb=False, record_pos=(0.074, 0.122), resolution=(1080, 1920)))
+        if cancelBtn:
+            touch(Template(r"tpl1525799136428.png", record_pos=(0.074, 0.122), resolution=(1080, 1920)))
+            sleep(1)
+        isLoacationAlert = False
+    print "location alter canceled ! "
+    return;
+
+
+def exitCaceledAccount():
     junkAccound=exists(Template(r"tpl1525773590239.png", record_pos=(-0.091, -0.749), resolution=(1080, 1920)))
     if junkAccound:
         touch(Template(r"tpl1525773638175.png", record_pos=(-0.427, -0.747), resolution=(1080, 1920)))
@@ -98,13 +114,16 @@ def shareToSubAccount():
     
         touch(Template(r"tpl1525773711606.png", record_pos=(0.237, 0.093), resolution=(1080, 1920)))
         sleep(1)
+
+    return;
+
+def shareToSubAccount():
+    cleanJunkAccount();
     
-    
-
-    
-
-
-
+    touch(Template(r"tpl1525451310507.png", record_pos=(0.209, -0.613), resolution=(1080.0, 1920.0)))
+    sleep(3)
+    cleanLocation();
+    exitCaceledAccount();
     touch(Template(r"tpl1525776327147.png", record_pos=(0.42, -0.742), resolution=(1080, 1920)))
     sleep(2)
 
@@ -116,7 +135,7 @@ def shareToSubAccount():
 
 
 
-    touch(Template(r"tpl1525776362573.png", record_pos=(-0.323, 0.17), resolution=(1080, 1920)))
+    touch(Template(r"tpl1525776362573.png", threshold=0.9, target_pos=5, rgb=False, record_pos=(-0.323, 0.17), resolution=(1080, 1920)))
     sleep(2)
 
 
@@ -135,10 +154,11 @@ def openSubAccount():
     keyevent("HOME")
     sleep(2)
     touch(Template(r"tpl1525776614728.png", record_pos=(0.163, -0.384), resolution=(1080, 1920)))
-    sleep(6)
+    sleep(15)
     return;
 
 def fellow():
+    
     received=exists(Template(r"tpl1525479334499.png", record_pos=(-0.38, -0.625), resolution=(1080.0, 1920.0)))
     if received:
 
@@ -146,7 +166,7 @@ def fellow():
         sleep(2)
 
 
-        card=exists(Template(r"tpl1525775234003.png", record_pos=(-0.245, -0.37), resolution=(1080, 1920)))
+        card=exists(Template(r"tpl1525775234003.png", threshold=0.9, target_pos=5, rgb=False, record_pos=(-0.245, -0.37), resolution=(1080, 1920)))
 
         if card:
             touch(Template(r"tpl1525479652540.png", threshold=0.25, target_pos=5, rgb=False, record_pos=(-0.176, -0.354), resolution=(1080, 1920)))
@@ -162,17 +182,24 @@ def fellow():
                 sleep(2)
         
         
-                fellowOk = exists(Template(r"tpl1525775432609.png", record_pos=(0.415, -0.756), resolution=(1080, 1920)))
+                # fellowOk = exists(Template(r"tpl1525775432609.png", record_pos=(0.415, -0.756), resolution=(1080, 1920)))
             
-                if fellowOk :
+                # if fellowOk :
                     # 返回子账号首页,删除消息.
-                    backToHomeTab()
+                    # backToHomeTab()
                     ########remove
-            enterAccount=exists(Template(r"tpl1525776785000.png", record_pos=(-0.001, 0.688), resolution=(1080, 1920)))
-            if enterAccount:
-                # 返回子账号首页,删除消息.
-                backToHomeTab()
-            ########remove
+            # else:
+                # backToHomeTab()
+                # enterAccount=exists(Template(r"tpl1525776785000.png", record_pos=(-0.001, 0.688), resolution=(1080, 1920)))
+                # if enterAccount:
+                #     # 返回子账号首页,删除消息.
+                #     ########remove
+                #     print "already  fellowed "
+                # freezAccount = exists(Template(r"tpl1525835169886.png", threshold=0.9, target_pos=5, rgb=False, record_pos=(0.031, -0.135), resolution=(1080, 1920)))
+                # if freezAccount:
+                #     touch(Template(r"tpl1525835207113.png", record_pos=(0.254, 0.094), resolution=(1080, 1920)))
+
+    backToHomeTabUseKeyEvent();
     return;
 def cancellationFellow():
     touch(Template(r"tpl1525451336035.png", record_pos=(0.425, -0.76), resolution=(1080.0, 1920.0)))
@@ -188,10 +215,15 @@ def cancellationFellow():
     return;
 
 
-        
+
+
+stop_app("com.tencent.mm")
+sleep(2)     
 #open main wx home tab
 openMainWX()
-backToHomeTab()
+
+
+()
 #share one account to sub count 
 shareToSubAccount()
 # open sub account 
--- airTest/wx_section.sh
+++ airTest/wx_section.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "start kill wx..."
-/Applications/AirtestIDE.app/Contents/MacOS/AirtestIDE runner "/Users/wyk/Desktop/aa/justKill.air"  --device Android://127.0.0.1:5037/SJE5T17515006720 --log "/Users/wyk/Desktop/1"
 
 echo "start loop ..."
-for i in {1..100}
+for i in {1..330}
 do
+#    /Applications/AirtestIDE.app/Contents/MacOS/AirtestIDE runner "/Users/wyk/Desktop/aa/justKill.air"  --device Android://127.0.0.1:5037/SJE5T17515006720 --log "/Users/wyk/Desktop/1"
     echo "working in $i ..."
     /Applications/AirtestIDE.app/Contents/MacOS/AirtestIDE runner "/Users/wyk/Desktop/aa/single_section.air"  --device Android://127.0.0.1:5037/SJE5T17515006720 --log "/Users/wyk/Desktop/1"
 done
